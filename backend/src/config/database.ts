5d33c0732cd3c32269ac9e2555732aefb2bc26a4
gustavo <gustavo.silva123913@gmail.com>
2026-08-21 00:12:08 -0300

    adiciona script de desenvolvimento e corrige a forma de obter a senha do banco de dados

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -6,7 +6,7 @@ export const DB = new Pool ({
     host: "localhost",
     port: 5432,
     user: process.env.POSTGRES_USER,
-    password: String(process.env.POSTGRES_PASSWORD),
+    password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB
 });
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import app from "./app";
 import { DB } from "./config/database";
 
+console.log(process.env.POSTGRES_USER);
+console.log(process.env.POSTGRES_DB);
 DB.query("SELECT NOW()")
   .then((resultado) => {
     console.log("Banco conectado:", resultado.rows[0]);
