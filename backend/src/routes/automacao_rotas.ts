450ac25509e8390e8fd75e47b79e095f16d0f664
gustavo <gustavo.silva123913@gmail.com>
2026-08-26 16:18:27 -0300

    adiciona autenticação e verificação de administrador na rota de cadastro de automação

--- backend/src/routes/automacao_rotas.ts
+++ backend/src/routes/automacao_rotas.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { cadastrarAutomacao } from "../controllers/automacao_controller";
+import { autenticar } from "../middlewares/routes_protect";
+import { exigirADM } from "../middlewares/admin_middlware";
 const router = Router();
-router.post("/", cadastrarAutomacao)
+router.post("/", autenticar, exigirADM, cadastrarAutomacao)
 export default router;
