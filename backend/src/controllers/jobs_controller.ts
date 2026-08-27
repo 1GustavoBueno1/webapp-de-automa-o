b7799c21b75d71f90c7f224eca95306052941766
gustavo <gustavo.silva123913@gmail.com>
2026-08-26 22:55:43 -0300

    corrige ordem dos parâmetros na função iniciarJOBs e ajusta rota de execução de JOBs

--- backend/src/controllers/jobs_controller.ts
+++ backend/src/controllers/jobs_controller.ts
@@ -4,12 +4,12 @@ import { cadastroJOB } from "../services/jobs_service";
 
 
 import { Response, Request } from "express";
-export async function iniciarJOBs(res: Response, req: Request) {
+export async function iniciarJOBs(req: Request, res: Response,) {
     const automacaoID = Number(req.params.id);
     const resultado = await cadastroJOB(automacaoID)
 
     if ("erro" in resultado) {
-        res.status(400).json({
+        return res.status(400).json({
             message: resultado.erro
         })
     }
--- backend/src/routes/automacao_rotas.ts
+++ backend/src/routes/automacao_rotas.ts
@@ -2,7 +2,10 @@ import { Router } from "express";
 import { cadastrarAutomacao } from "../controllers/automacao_controller";
 import { autenticar } from "../middlewares/routes_protect";
 import { exigirADM } from "../middlewares/admin_middlware";
+import { iniciarJOBs } from "../controllers/jobs_controller";
+
 
 const router = Router();
 router.post("/", autenticar, exigirADM, cadastrarAutomacao)
+router.post("/:id/executar", autenticar, iniciarJOBs)
 export default router;
